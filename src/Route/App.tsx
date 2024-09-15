337c058a2478213f2819b4bfaf933696031fbc09
unknown <you@example.com>
2024-09-15 12:58:22 +0300

    updt

--- src/Route/App.tsx
+++ src/Route/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Catalog from './pages/Catalog';
-import Cart from './pages/Cart';
+import Catalog from '../pages/Catalog';
+import Cart from '../pages/Cart';
 
 const App: React.FC = () => {
   return (
