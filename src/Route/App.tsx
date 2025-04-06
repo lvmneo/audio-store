0ea7d7365e367120fa1bf7b13f179904862df51d
lvmneo <pimknim@gmail.com>
2025-04-06 16:52:59 +0300

    updt

--- src/Route/App.tsx
+++ src/Route/App.tsx
@@ -5,13 +5,17 @@ import Cart from '../pages/Cart';
 
 const App: React.FC = () => {
   return (
+   
     <Router>
+       
       <Routes>
         <Route path="/" element={<Catalog />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
+    
   );
+
 };
 
 export default App;
